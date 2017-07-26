larsks/ax25-tools

commit 71918b7d4e95f6ccf7b4d90eb7538c3406c5fb05

axwrapper: Mark functions sigchld_handler and convert_{cr_lf,lf_cr} static.
This results in a new warning about sigchld_handler() being unused which
is because the caller is currently commented out which we leave to be
sorted out later.

Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

ax25/axwrapper.c
@@ -46,13 +46,13 @@
 #define	PERROR(s)	fprintf(stderr, "*** %s: %s\r", (s), strerror(errno))
 #define	USAGE()		fputs("Usage: axwrapper [-p <paclen>] <filename> <argv[0]> ...\r", stderr)
 
-void sigchld_handler(int sig)
+static void sigchld_handler(int sig)
 {
 	/* fprintf(stderr, "Caught SIGCHLD, exiting...\r"); */
 	exit(0);
 }
 
-void convert_cr_lf(unsigned char *buf, int len)
+static void convert_cr_lf(unsigned char *buf, int len)
 {
 	while (len-- > 0) {
 		if (*buf == '\r') *buf = '\n';
@@ -60,7 +60,7 @@ void convert_cr_lf(unsigned char *buf, int len)
 	}
 }
 
-void convert_lf_cr(unsigned char *buf, int len)
+static void convert_lf_cr(unsigned char *buf, int len)
 {
 	while (len-- > 0) {
 		if (*buf == '\n') *buf = '\r';

commit bfe071e3ac625e8546de6444e89adc2a90655a63

axwrapper: Remove unused code resulting in warning.
gcc  -g -O2 -Wall   -o axspawn axspawn.o access.o md5.o -lax25 -lutil
gcc -DHAVE_CONFIG_H -I. -I..  -D_GNU_SOURCE -DAX25_SYSCONFDIR=\""/usr/local/etc/ax25/"\" -DAX25_LOCALSTATEDIR=\""/us
r/local/var/ax25/"\"   -g -O2 -Wall -MT axwrapper.o -MD -MP -MF .deps/axwrapper.Tpo -c -o axwrapper.o axwrapper.c
axwrapper.c:49:13: warning: ‘sigchld_handler’ defined but not used [-Wunused-function]
 static void sigchld_handler(int sig)
             ^~~~~~~~~~~~~~~

Since 71918b7d4e95 ("axwrapper: Mark functions sigchld_handler and
convert_{cr_lf,lf_cr} static.") made sigchld_handler() static gcc was
warning about the function being unused.  No point in keeping this around;
SIGCHLD is perfectly fine.

Fixes: 71918b7d4e95 ("axwrapper: Mark functions sigchld_handler and convert_{cr_lf,lf_cr} static.")
Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

ax25/axwrapper.c
@@ -46,12 +46,6 @@
 #define	PERROR(s)	fprintf(stderr, "*** %s: %s\r", (s), strerror(errno))
 #define	USAGE()		fputs("Usage: axwrapper [-p <paclen>] <filename> <argv[0]> ...\r", stderr)
 
-static void sigchld_handler(int sig)
-{
-	/* fprintf(stderr, "Caught SIGCHLD, exiting...\r"); */
-	exit(0);
-}
-
 static void convert_cr_lf(unsigned char *buf, int len)
 {
 	while (len-- > 0) {
@@ -118,7 +112,6 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	/* signal(SIGCHLD, sigchld_handler); */
 	signal(SIGCHLD, SIG_IGN);
 
 	pid = fork();